b5fcd8365ce64a8bd79122064ae54ee01211396f
penoplavaan <alex@penoplavaan.space>
2022-12-26 18:12:28 +0500

    feat: change baseUrl for prod

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8040/api",
+  baseURL: "http://158.160.47.208/:8040/api",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*"
