180249685c0b8478e20bd73cd81259755e57571c
penoplavaan <alex@penoplavaan.space>
2022-12-28 23:50:35 +0600

    feat: change proxy and baseURL

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://51.250.90.3:8040/api",
+  baseURL: "http://51.250.88.54:8040/api",
   headers: {
     "Content-type": "application/json",
     "Access-Control-Allow-Origin": "*"
